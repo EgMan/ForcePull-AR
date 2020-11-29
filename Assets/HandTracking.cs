16ebc57bf83584c6344994ab740da4dd7c80d680
Antonio Woods <antonio95woods@gmail.com>
2020-11-29 07:01:08 -0500

    Integrate hand tracking

---
+++ Assets/HandTracking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    public GameObject debug;
+    void Update()
+    {
+        HandInfo hand = ManomotionManager.Instance.Hand_infos[0].hand_info;
+        GestureInfo gesture = hand.gesture_info;
+        TrackingInfo tracking = hand.tracking_info;
+
+        Debug.Log("palm center "+tracking.palm_center.ToString());
+        // transform.position = tracking.palm_center;
+        transform.position = ManoUtils.Instance.CalculateNewPosition(tracking.palm_center, tracking.depth_estimation);
+        switch (gesture.mano_gesture_continuous)
+        {
+            case ManoGestureContinuous.CLOSED_HAND_GESTURE:
+                break;
+            default:
+                break;
+        }
+
+        var a = debug.GetComponent<Text>();
+        a.text = "palm center:"+transform.position.ToString() + "  gesture:"+gesture.mano_gesture_continuous;
+    }
+}
