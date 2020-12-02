d646d5a226ebe89027d05b40c0bce9197c44266d
Antonio Woods <antonio95woods@gmail.com>
2020-12-02 07:22:48 -0500

    Implement smoothing logic for hand tracking

--- Assets/CursorTracking.cs
+++ Assets/CursorTracking.cs
@@ -10,6 +10,10 @@ public class CursorTracking : MonoBehaviour
     public bool forcePull = false;
     private Vector3 initialPos;
     private MeshRenderer mesh;
+    private List<Vector3> handPositionBuffer = new List<Vector3>();
+    private int handPositionBufferMaxSize = 4;
+    private List<ManoGestureContinuous> handGestureBuffer = new List<ManoGestureContinuous>();
+    private int handGestureBufferMaxSize = 4;
     private void Start()
     {
         initialPos = transform.localPosition;
@@ -29,10 +33,43 @@ public class CursorTracking : MonoBehaviour
             }
         }
 
-        if (gesture.mano_gesture_continuous != ManoGestureContinuous.NO_GESTURE && gesture.mano_gesture_continuous != 0)
+        // tracking smoothing
+        if (handGestureBuffer.Count >= handGestureBufferMaxSize)
         {
-            transform.position = ManoUtils.Instance.CalculateNewPosition(tracking.palm_center, tracking.depth_estimation);
-            if (gesture.mano_gesture_continuous == ManoGestureContinuous.CLOSED_HAND_GESTURE)
+            handGestureBuffer.RemoveAt(0);
+        }
+        handGestureBuffer.Add(gesture.mano_gesture_continuous);
+        ManoGestureContinuous smoothedGesture = ManoGestureContinuous.NO_GESTURE;
+        foreach (ManoGestureContinuous ges in handGestureBuffer)
+        {
+            if (ges == ManoGestureContinuous.CLOSED_HAND_GESTURE)
+            {
+                smoothedGesture = ManoGestureContinuous.CLOSED_HAND_GESTURE;
+                break;
+            }
+            if (ges == ManoGestureContinuous.OPEN_HAND_GESTURE)
+            {
+                smoothedGesture = ManoGestureContinuous.OPEN_HAND_GESTURE;
+            }
+        }
+
+        if (smoothedGesture != ManoGestureContinuous.NO_GESTURE)
+        {
+            // tracking smoothing
+            if (handPositionBuffer.Count >= handPositionBufferMaxSize)
+            {
+                handPositionBuffer.RemoveAt(0);
+            }
+            handPositionBuffer.Add(ManoUtils.Instance.CalculateNewPosition(tracking.palm_center, tracking.depth_estimation));
+            Vector3 smoothedPosition = Vector3.zero;
+            foreach (Vector3 pos in handPositionBuffer)
+            {
+                smoothedPosition += pos;
+            }
+            smoothedPosition /= handPositionBuffer.Count;
+
+            transform.position = smoothedPosition;
+            if (smoothedGesture == ManoGestureContinuous.CLOSED_HAND_GESTURE)
             {
                 forcePull = true;
             }
@@ -56,7 +93,7 @@ public class CursorTracking : MonoBehaviour
             mesh.material = unselected;
         }
 
-        // var a = debug.GetComponent<Text>();
-        // a.text = "palm center:"+transform.position.ToString() + "  gesture:"+gesture.mano_gesture_continuous;
+        var a = debug.GetComponent<Text>();
+        a.text = "palm center:"+transform.position.ToString() + "  gesture:"+gesture.mano_gesture_continuous;
     }
 }
